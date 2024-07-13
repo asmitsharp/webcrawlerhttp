5d0ff27529bf916d5babaa9b802144c71e664a38
Asmit Singh <ashmitsingh068@gmail.com>
2024-07-13 23:57:25 +0530

    crawl page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello world")
+const { crawlPage } = require("./crawl")
+
+function main() {
+  if (process.argv.length < 3) {
+    console.log("no website provided")
+    process.exit(1)
+  }
+  if (process.argv.length > 3) {
+    console.log("too many command line arguments")
+    process.exit(1)
+  }
+
+  const baseUrl = process.argv[2]
+  console.log(`starting crawl of ${baseUrl}`)
+  crawlPage(baseUrl)
+}
+
+main()
